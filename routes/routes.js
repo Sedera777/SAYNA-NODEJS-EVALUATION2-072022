f930425a852a540adaf5f5d22f179f49cf09ea7c
Sedera777 <S.Randrianarivolagmail.com>
2022-07-27 14:33:07 +0300

    updating data...

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,18 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(session({
+    secret: 'sayna',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+router.use(require('../middlewares/flash'))
 router.get('/backend', (req, res) => {
     res.sendFile(path.resolve('views/backend.html'));
 })
@@ -24,6 +35,17 @@ router.get('/signup', (req, res) => {
 })
 
 router.get('/contact', (req, res) => {
-    res.sendFile(path.resolve('views/contact.html'));
+    res.render('contact');
+})
+
+router.post('/contact', (req, res) => {
+    if (req.body.lastname === '' || req.body.firstname === '' || req.body.avis === '' || req.body.note === '') {
+        req.flash('error', 'Vous n\'avez pas rempli tous les champs! 😢')
+        res.redirect('/contact')
+    } else {
+        req.flash('success', 'Merci pour votre avis!!!')
+        res.redirect('/contact')
+    }
+
 })
 module.exports = router
